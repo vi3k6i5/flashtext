241b158ea6368f0a5c4e1aa3dd72666cc1eafa2c
Rémi <remi.adon@gmail.com>
2019-06-01 15:29:08 +0200

    first implementation of fuzzy matching via leventhstein, first tests with extract_keywords

---
+++ test/test_extract_fuzzy.py
@@ -0,0 +1,113 @@
+from flashtext import KeywordProcessor
+import logging
+import unittest
+import json
+import re
+
+logger = logging.getLogger(__name__)
+
+class TestKeywordReplacer(unittest.TestCase):
+    def setUp(self):
+        logger.info("Starting...")
+
+    def tearDown(self):
+        logger.info("Ending.")
+
+    def test_extract_deletion(self):
+        """
+        Fuzzy deletion
+        """
+        keyword_proc = KeywordProcessor()
+        for keyword in (('skype', 'messenger'), ):
+            keyword_proc.add_keyword(*keyword)
+
+        sentence = "hello, do you have skpe ?"
+        extracted_keywords = [('messenger', 19, 23)]
+        self.assertEqual(keyword_proc.extract_keywords(sentence, span_info=True, max_cost=1), extracted_keywords)
+
+
+    def test_extract_addition(self):
+        """
+        Fuzzy addition
+        """
+        keyword_proc = KeywordProcessor()
+        for keyword in (('colour here', 'couleur ici'), ('and heere', 'et ici')):
+            keyword_proc.add_keyword(*keyword)
+
+        sentence = "color here blabla and here"
+
+        extracted_keywords = [('couleur ici', 0, 10), ('et ici', 18, 26)]
+        self.assertListEqual(keyword_proc.extract_keywords(sentence, span_info=True, max_cost=1), extracted_keywords)
+
+
+    def test_correct_keyword_on_addition(self):
+        keyword_proc = KeywordProcessor()
+        for keyword in (('colour here', 'couleur ici'), ('and heere', 'et ici')):
+            keyword_proc.add_keyword(*keyword)
+
+        current_dict = keyword_proc.keyword_trie_dict['c']['o']['l']['o']
+        closest_node, cost, depth = next(
+            keyword_proc.levensthein('r', max_cost=1, start_node=current_dict),
+            ({}, 0, 0)
+            )
+        self.assertDictEqual(closest_node, current_dict['u']['r'])
+        self.assertEqual(cost, 1)
+        self.assertEqual(depth, 2)
+
+        current_dict_continued = {'e' : {'e': {'r': {'e': {'_keyword_': 'et ici'}}}}}
+        closest_node, cost, depth = next(
+            keyword_proc.levensthein('ere', max_cost=1, start_node=current_dict_continued),
+            ({}, 0, 0),
+        )
+        self.assertDictEqual(closest_node, current_dict_continued['e']['e']['r']['e'])
+        self.assertEqual(cost, 1)
+        self.assertEqual(depth, 4)
+
+
+    def test_correct_keyword_on_deletion(self):
+        keyword_proc = KeywordProcessor()
+        keyword_proc.add_keyword('skype')
+        current_dict = {'y': {'p': {'e': {'_keyword_': 'skype'}}}}
+
+        closest_node, cost, depth = next(
+            keyword_proc.levensthein('pe', max_cost=1, start_node=current_dict),
+            ({}, 0, 0),
+        )
+
+        self.assertDictEqual(closest_node, current_dict['y']['p']['e'])
+        self.assertEqual(cost, 1)
+        self.assertEqual(depth, 3)
+
+    def test_extract_cost_spread_over_multiple_words(self):
+        """
+        Here we try to extract a keyword make of different words
+        the current cost should be decreased by one when encountering 'maade' (1 insertion)
+        and again by one when encountering 'multple' (1 deletion)
+        """
+        keyword_proc = KeywordProcessor()
+        keyword_made_of_multiple_words = 'made of multiple words'
+        keyword_proc.add_keyword(keyword_made_of_multiple_words)
+        sentence = "this sentence contains a keyword maade of multple words"
+
+        #current_dict = keyword_proc.keyword_trie_dict['m']['a']
+        #closest_node, cost, depth = keyword_proc._correct_word('ade of multiple words')
+        #self.assertDictEqual()
+
+        extracted_keywords = [(keyword_made_of_multiple_words, 33, 55)]
+        self.assertEqual(keyword_proc.extract_keywords(sentence, span_info=True, max_cost=2), extracted_keywords)
+
+
+    def test_extract_multiple_keywords(self):
+        keyword_proc = KeywordProcessor()
+        keyword_proc.add_keyword('first keyword')
+        keyword_proc.add_keyword('second keyword')
+        sentence = "starts with a first kyword then add a secand keyword"
+        extracted_keywords = [
+            ('first keyword', 14, 26),
+            ('second keyword', 38, 52),
+        ]
+        self.assertEqual(keyword_proc.extract_keywords(sentence, span_info=True, max_cost=1), extracted_keywords)
+
+
+if __name__ == '__main__':
+    unittest.main()
