bf3b6895e9ff1b1cb60d1ae7b0a93d886762cc97
Rémi <remi.adon@gmail.com>
2019-06-06 17:02:35 +0200

    fuzzy matching: [ADD] test for get_next_word

---
+++ test/test_kp_next_word.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from flashtext import KeywordProcessor
+import logging
+import unittest
+import json
+import re
+import sys
+
+logger = logging.getLogger(__name__)
+
+class TestKPNextWord(unittest.TestCase):
+    def setUp(self):
+        logger.info("Starting...")
+
+    def tearDown(self):
+        logger.info("Ending.")
+
+    def test_next_word(self):
+        """
+        Test for next word extraction
+        """
+        keyword_proc = KeywordProcessor()
+        self.assertEqual(keyword_proc.get_next_word(''), '')
+        self.assertEqual(keyword_proc.get_next_word('random sentence'), 'random')
+        self.assertEqual(keyword_proc.get_next_word(' random sentence'), '')
+
+if __name__ == '__main__':
+    unittest.main()
